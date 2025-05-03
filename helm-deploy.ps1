8e611dd3979555618661c614c94a1ed862f6e738
zeliak <kis.oliver@outlook.hu>
2025-05-03 21:54:38 +0200

    adding new deploy

--- helm-deploy.ps1
+++ helm-deploy.ps1
@@ -1,6 +1,6 @@
 helm upgrade --install chatoverlay `
     --namespace chatoverlay `
     --create-namespace `
-    --set frontendImage.tag="v0.1.12" `
-    --set backendImage.tag="v0.1.12" `
+    --set frontendImage.tag="v0.1.13" `
+    --set backendImage.tag="v0.1.13" `
     .\helm\chatoverlay
