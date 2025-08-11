ff7fc0925d2279d195101ad4a44f5dcd4ed98c71
zeliak <kis.oliver@outlook.hu>
2025-08-12 00:25:22 +0200

    changing deploy to new cluster [fev:0.1.25][bev:0.1.25]

--- helm-render.ps1
+++ helm-render.ps1
@@ -2,5 +2,6 @@ helm template chatoverlay `
     --namespace chatoverlay `
     --create-namespace `
     --set backend.skip=false `
-    --set frontend.skip=true `
+    --set frontend.skip=false `
+    --set-json 'frontendIngress.tls=[{"hosts":["chatoverlay-medve.etmbp.lo"],"secretName":"chatoverlay-medve-tls"}]' `
     .\helm\chatoverlay
